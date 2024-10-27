a84a94b93244a08c83de52c5bf4af9105f995008
hanyuu <hanyuujp5192@protonmail.com>
2024-10-27 20:49:09 +0000

    inclusion of global kernel_list_directory chararray + fetching

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <limits.h>
+#include <stdbool.h>
 
 #include "hyk_commands.h"
 #include "hyk_filehandling.h"
@@ -23,11 +23,33 @@ int main(int argc, char **argv)
 	e_command current_command;
 
 	if ( file_handling(argc, argv) == EXIT_FAILURE )
-    {
         return EXIT_FAILURE;
+
+    FILE * config_file = fopen("/etc/opt/hykernel/config/config", "r");
+
+    char buffer[64];
+    fgets(buffer, sizeof(buffer), config_file); // gets hykernel_directory string
+    fgets(buffer, sizeof(buffer), config_file); // gets kernel_directory string
+
+    char current_char = 0;
+    int i = 0;
+
+    while ( (current_char = buffer[i]) != '=' )
+        i++;
+
+    i++;
+    for (int j = 0; j < 128; j++ )
+    {
+        kernel_list_directory[j] = buffer[i];
+        i++;
     }
 
-	current_command = get_command(argc, argv);
+    fclose(config_file);
+
+    // remove endline character
+    kernel_list_directory[strlen(kernel_list_directory) - 1] = '\x00';
+
+	current_command = get_command(argv);
 	run_command(current_command);
 
 	return 0;
