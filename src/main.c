4300fc6187f4efcbe6c023ac8e30c62c1cee1e7a
hanyuu <hanyuujp5192@protonmail.com>
2024-10-27 16:31:46 +0000

    complete removal of deprecated readlink() code

--- src/main.c
+++ src/main.c
@@ -22,35 +22,6 @@ int main(int argc, char **argv)
 {
 	e_command current_command;
 
-    /*
-	char * hykd_buffer = (char *)malloc(128 * sizeof(char));
-
-	ssize_t executable_len = readlink(
-		"/proc/self/exe",
-		hykd_buffer,
-		sizeof(hykernel_directory) - 1
-	);
-	
-	char * lso_ptr = strrchr(hykd_buffer, '/');
-	*(lso_ptr + 1) = '\x00';
-	strcpy(hykernel_directory, hykd_buffer);
-
-	free(hykd_buffer);
-
-	if ( executable_len != -1 )
-	{
-		hykernel_directory[executable_len] = '\x00';	
-		printf("exec path: %s\n", hykernel_directory);
-	}
-	else
-	{
-		perror("readlink");
-		return EXIT_FAILURE;
-	}
-    */
-
-    
-
 	if ( file_handling(argc, argv) == EXIT_FAILURE )
     {
         return EXIT_FAILURE;
